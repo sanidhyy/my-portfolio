cc5119e149d6d3bc1a90301a9d5bd83b6987e9de
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-07-22 22:20:43 +0530

    svgs updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 
+// Sub Components
+import PowerButton from "../subComponents/PowerButton";
+
 const MainContainer = styled.div`
   background: ${(props) => props.theme.body};
   width: 100vw;
   height: 100vh;
+  overflow: hidden;
+  position: relative;
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Karla", sans-serif;
+    font-weight: 500;
+  }
+`;
+
+const Container = styled.div`
+  padding: 2rem;
 `;
 
 const Main = () => {
   return (
     <MainContainer>
-      <h1>Home</h1>
+      <Container>
+        <PowerButton />
+      </Container>
     </MainContainer>
   );
 };
