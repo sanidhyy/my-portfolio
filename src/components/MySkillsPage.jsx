089db552af3b5004052108a602cd0fda11bffb76
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-07-31 21:50:14 +0530

    my skills page done

--- src/components/MySkillsPage.jsx
+++ src/components/MySkillsPage.jsx
@@ -1,7 +1,140 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { LightTheme } from "./Themes";
+import { Design, Develope } from "./AllSvgs";
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import Particle from "../subComponents/Particle";
+
+const Main = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  font-family: "Ubuntu Mono", monospace;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  cursor: pointer;
+
+  &:hover {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
+  }
+`;
+
+const Title = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1em + 1vw);
+
+  ${Box}:hover & {
+    & > * {
+      fill: ${(props) => props.theme.body};
+    }
+  }
+
+  & > *:first-child {
+    margin-right: 1rem;
+  }
+`;
+
+const Description = styled.div`
+  color: ${(props) => props.theme.text};
+  font-size: calc(1vw + 0.6em);
+  padding: 0.5rem 0;
+
+  ${Box}:hover & {
+    color: ${(props) => props.theme.body};
+  }
+
+  strong {
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+  }
+
+  ul,
+  p {
+    margin-left: 2rem;
+  }
+`;
 
 const MySkillsPage = () => {
-  return <div>MySkillsPage</div>;
+  return (
+    <ThemeProvider theme={LightTheme}>
+      <Main>
+        <LogoComponent theme="light" />
+        <SocialIcons theme="light" />
+        <PowerButton />
+        <Particle theme="light" />
+        <Box>
+          <Title>
+            <Design width={40} height={40} /> Designer
+          </Title>
+
+          <Description>
+            I love to create design which speaks, Keep it clean, minimal and
+            simple.
+          </Description>
+          <Description>
+            <strong>I like to Design</strong>
+            <ul>
+              <li>Web Design</li>
+              <li>Mobile Apps</li>
+            </ul>
+          </Description>
+          <Description>
+            <strong>Tools</strong>
+            <ul>
+              <li>Figma</li>
+            </ul>
+          </Description>
+          <Description>
+            <strong>I like to Design</strong>
+            <ul>
+              <li>Web Design</li>
+              <li>Mobile Apps</li>
+            </ul>
+          </Description>
+        </Box>
+        <Box>
+          <Title>
+            <Develope width={40} height={40} /> Frontend Developer
+          </Title>
+          <Description>
+            I value business or brand for which i'm creating, thus i enjoy
+            bringing new ideas to life.
+          </Description>
+          <Description>
+            <strong>Skills</strong>
+            <p>
+              Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase
+              etc.
+            </p>
+          </Description>
+          <Description>
+            <p>VScode, Github, Codepen etc.</p>
+          </Description>
+        </Box>
+      </Main>
+    </ThemeProvider>
+  );
 };
 
 export default MySkillsPage;
