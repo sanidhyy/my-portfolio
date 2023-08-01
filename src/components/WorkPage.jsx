2450f35f865508985b0131d0e199592c7edac0bb
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-08-01 08:43:43 +0530

    scroll animations added

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled, { ThemeProvider } from "styled-components";
 
 import { DarkTheme } from "./Themes";
@@ -7,11 +7,11 @@ import SocialIcons from "../subComponents/SocialIcons";
 import PowerButton from "../subComponents/PowerButton";
 import { Work } from "../data/WorkData";
 import Card from "../subComponents/Card";
+import { YinYang } from "./AllSvgs";
 
 const Main = styled.div`
   background-color: ${(props) => props.theme.body};
-  width: 100vw;
-  height: 100vh;
+  height: calc(45vh * ${Work.length});
   position: relative;
   overflow: hidden;
 `;
@@ -25,7 +25,33 @@ const Box = styled.div`
   color: #fff;
 `;
 
+const Rotate = styled.span`
+  display: block;
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+  width: 80px;
+  height: 80px;
+  z-index: 1;
+`;
+
 const WorkPage = () => {
+  const mainRef = useRef(null);
+  const yinYangRef = useRef(null);
+
+  useEffect(() => {
+    let mainElement = mainRef.current;
+    let yinYangElement = yinYangRef.current;
+    const rotate = () => {
+      mainElement.style.transform = `translateX(${-window.scrollY}px)`;
+
+      yinYangElement.style.transform = `rotate(${-window.scrollY}deg)`;
+    };
+
+    window.addEventListener("scroll", rotate);
+    return () => window.removeEventListener("scroll", rotate);
+  }, []);
+
   return (
     <ThemeProvider theme={DarkTheme}>
       <Main>
@@ -33,11 +59,20 @@ const WorkPage = () => {
         <SocialIcons theme="dark" />
         <PowerButton />
 
-        <Box>
+        <Box ref={mainRef}>
           {Work.map((data) => (
             <Card key={data.id} data={data} />
           ))}
         </Box>
+
+        <Rotate ref={yinYangRef}>
+          <YinYang
+            ref={yinYangRef}
+            width={80}
+            height={80}
+            fill={DarkTheme.text}
+          />
+        </Rotate>
       </Main>
     </ThemeProvider>
   );
