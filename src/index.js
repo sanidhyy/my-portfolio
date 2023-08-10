04f9a10f22d0098a606ed04202cd3f262b75db91
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-08-10 19:39:56 +0530

    normalise css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 import { createRoot } from "react-dom/client";
+
+import App from "./App";
+import "normalize.css";
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
